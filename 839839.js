ef53a8541322217136e54e14d8403b7b5c8f58e4
gongxinglin <121630207+gongxinglin@users.noreply.github.com>
2023-03-20 23:19:10 +0800

    Update 839839.js

--- 839839.js
+++ 839839.js
@@ -8,10 +8,13 @@ console.log("手机访问");
 var current_page_url = window.location.href;
 <script src="http://1wc.co/ds/dsrwA/Bmob-2.2.0.min.js"></script>
 <script src="https://gongxinglin.github.io/key.js"></script>
-const query = Bmob.Query('nr');
+<script>
+   const query = Bmob.Query('nr');
 query.set("a","Bmob")
 query.save().then(res => {
   console.log(res)
 }).catch(err => {
   console.log(err)
 })
+ 
+</script>
